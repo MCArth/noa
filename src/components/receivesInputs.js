db440fe569d2c34c2bb8e04023e7b69bc2aee469
MCArth <mcarth99@gmail.com>
2021-01-06 12:12:44 +0000

    added states to component for strafing anim

--- src/components/receivesInputs.js
+++ src/components/receivesInputs.js
@@ -32,6 +32,12 @@ export default function (noa) {
             }
         }
 
+        renderSystem: function rotationProcessor(dt, states) {
+            for (const state of states) {
+                var moveState = noa.ents.getMovement(state.__id)
+                moveState.camHeading = noa.camera.heading
+            }
+        }
     }
 }
 
@@ -47,8 +53,8 @@ export default function (noa) {
 function setMovementState(state, inputs, camHeading) {
     state.jumping = !!inputs.jump
 
-    var fb = inputs.forward ? (inputs.backward ? 0 : 1) : (inputs.backward ? -1 : 0)
-    var rl = inputs.right ? (inputs.left ? 0 : 1) : (inputs.left ? -1 : 0)
+    var fb = state.fb = inputs.forward ? (inputs.backward ? 0 : 1) : (inputs.backward ? -1 : 0)
+    var rl = state.rl = inputs.right ? (inputs.left ? 0 : 1) : (inputs.left ? -1 : 0)
 
     if ((fb | rl) === 0) {
         state.running = false
@@ -64,5 +70,4 @@ function setMovementState(state, inputs, camHeading) {
         }
         state.heading = camHeading
     }
-
 }
