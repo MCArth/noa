6ae998428e8b4d377fe617bb6c9f70411b30e664
MCArth <mcarth99@gmail.com>
2021-01-15 10:43:26 +0000

    changes so that crouching works

--- src/lib/inputs.js
+++ src/lib/inputs.js
@@ -11,26 +11,47 @@ var defaultOptions = {
 }
 
 var defaultBindings = {
-    "forward": ["W", "<up>"],
-    "left": ["A", "<left>"],
-    "backward": ["S", "<down>"],
-    "right": ["D", "<right>"],
-    "fire": "<mouse 1>",
-    "mid-fire": ["<mouse 2>", "Q"],
-    "alt-fire": ["<mouse 3>", "E"],
-    "jump": "<space>",
-    "sprint": "<shift>",
-    "crouch": "<control>",
+    bindings: {
+        "forward": ["W", "<up>"],
+        "left": ["A", "<left>"],
+        "backward": ["S", "<down>"],
+        "right": ["D", "<right>"],
+        "fire": "<mouse 1>",
+        "mid-fire": ["<mouse 2>", "Q"],
+        "alt-fire": ["<mouse 3>", "E"],
+        "jump": "<space>",
+        "sprint": "<shift>",
+        "crouch": ["Z", "<caps-lock>"],
+    }
 }
 
-/**
- * @internal
- * @returns {Inputs}
- */
-export function createInputs(noa, opts, element) {
-    opts = Object.assign({}, defaultOptions, opts)
-    var inputs = makeInputs(element, opts)
-    var b = opts.bindings || defaultBindings
+
+
+//Prevent Ctrl+S (and Ctrl+W for old browsers and Edge)
+document.onkeydown = function (e) {
+    e = e || window.event;//Get event
+
+    var code = e.which || e.keyCode //Get key code
+
+    if (!e.ctrlKey) return;
+
+
+    switch (code) {
+        case 220: 
+        case 83://Block Ctrl+S
+        case 87://Block Ctrl+W -- Not work in Chrome and new Firefox
+            console.log("yee")
+            e.preventDefault()
+            e.stopPropagation()
+            break;
+    }
+};
+
+
+function makeInputs(noa, opts, element) {
+    opts = Object.assign({}, defaultBindings, opts)
+    var inputs = createInputs(element, opts)
+    var b = opts.bindings
     for (var name in b) {
         var arr = (Array.isArray(b[name])) ? b[name] : [b[name]]
         arr.unshift(name)
