1c58c502c177f23aedc02408eefe998a04b9cc49
Arthur <mcarth99@gmail.com>
2023-07-08 20:44:19 +0300

    ping voxel-physics-engine

--- src/lib/shims.js
+++ src/lib/shims.js
@@ -3,7 +3,7 @@
  * This works around some old node-style code in a
  * dependency of box-intersect.
 */
-if (window && !window['global']) {
+if (typeof window !== "undefined" && !window['global']) {
     window['global'] = window.globalThis || {}
 }
 
